056d1819ac5758f03bcc88abe6ba86ab08399917
Daniel <jenwaridaniel123@gmail.com>
2022-10-17 15:55:26 +0100

    used

---
+++ Apple/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        bad:['Monoton']
+      },
+      fontFamily:{
+        Daniel:['Roboto Condensed']
+      },
+      fontFamily:{
+        sam:['Major Mono Display']
+      },
+      fontfamily:{
+        Ossy:['Oswald']
+      },
+      fontFamily:{
+        card:['Comforter Brush']
+      },
+      fontfamily:{
+        duck:['Ubuntu']
+      },
+      fontFamily:{
+        alex:['Major Mono Display']
+      },
+      fontFamily:{
+        Arials:['IBM Plex Mono']
+      }
+    },
+  },
+  plugins: [],
+}
